87dbe8bb753b164d406ea9c04dcdf598a6794b85
Yeonho Lee <tomatoyeonho@naver.com>
2020-04-20 21:20:07 +0900

    add vote model

--- vote/admin.py
+++ vote/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import VoteTopic, VoteSelection
 
 # Register your models here.
+
+admin.site.register(VoteTopic)
+admin.site.register(VoteSelection)
--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+
+class VoteTopic(models.Model):
+    title = models.CharField(max_length=255, verbose_name=u'투표 제목', null=False, default=u'제목이 없는 투표')
+
+class VoteSelection(models.Model):
+    name = models.CharField(max_length=255, verbose_name=u'투표 선택지 이름', null=False, default='제목이 없는 선택지')
+    topic = models.ForeignKey(VoteTopic, verbose_name='소속 투표 주제', on_delete=models.CASCADE)
+    votedUsers = models.ManyToManyField(User)
