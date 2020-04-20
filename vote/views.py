dff60c2043d1edbf16e718e4c724d83344cc8157
Yeonho Lee <tomatoyeonho@naver.com>
2020-04-20 22:03:25 +0900

    add vote list view

--- vote/views.py
+++ vote/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import VoteTopic
 
 # Create your views here.
+def main(request):
+    openVotes = VoteTopic.objects.filter(is_closed=False)
+    return render(request, 'vote/main.html', {"openVotes": openVotes})
