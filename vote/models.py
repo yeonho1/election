e362e24cc4b60830ab3208b57e545f36be26f859
Yeonho Lee <tomatoyeonho@naver.com>
2020-04-21 20:52:08 +0900

    add str, unicode method to votetopic and voteselection model

--- vote/models.py
+++ vote/models.py
@@ -9,7 +9,22 @@ class VoteTopic(models.Model):
     who_opened = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     is_closed = models.BooleanField(default=False, verbose_name=u'마감 여부')
 
+    def __unicode__(self):
+        result = u'{} (by {})'.format(self.title, self.who_opened)
+        if self.is_closed:
+            result += u' (마감됨)'
+        return result
+
+    def __str__(self):
+        return self.__unicode__()
+
 class VoteSelection(models.Model):
     name = models.CharField(max_length=255, verbose_name=u'투표 선택지 이름', null=False, default='제목이 없는 선택지')
     topic = models.ForeignKey(VoteTopic, verbose_name='소속 투표 주제', on_delete=models.CASCADE)
     votedUsers = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return u'{}의 선택지 \'{}\''.format(str(self.topic), self.name)
+
+    def __str__(self):
+        return self.__unicode__()
