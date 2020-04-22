267a4d48264a5cc879f506b94a4c21badca401fb
Yeonho Lee <tomatoyeonho@naver.com>
2020-04-22 19:36:28 +0900

    add login feature

--- vote/views.py
+++ vote/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from .models import VoteTopic, VoteSelection
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def main(request):
+    user = request.user
+    loggedin = False
+    if user.id is not None:
+        loggedin = True
     openVotes = VoteTopic.objects.filter(is_closed=False)
-    return render(request, 'vote/main.html', {"openVotes": openVotes})
+    return render(request, 'vote/main.html', {"openVotes": openVotes, 'user': user, 'log': loggedin})
 
 def viewvote(request, id):
     try:
@@ -16,3 +21,16 @@ def viewvote(request, id):
     for sel in selections:
         voted += sel.votedUsers.count()
     return render(request, 'vote/view.html', {'topic': topic, 'selections': selections, 'voted': voted})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(nickname=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('votemain')
+        else:
+            return HttpResponse('로그인에 실패하였습니다. 다시 시도해 주세요.')
+    else:
+        return render(request, 'vote/login.html', {})
